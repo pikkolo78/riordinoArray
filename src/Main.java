f91ce24d1b8ea258f0989be4f353aac3ad673503
pikkolo78 <antoniodacc1978@gmail.com>
2017-10-01 23:39:41 +0200

    riordino

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+      int[] array={25,14,45,32,12,3,5,6,89,23};
+              int numeroPicccolo;
+              int valoreAttuale=0;
+              int numeroProvvisorio;
+              for (int i = 0; i<array.length; i++) {
+            numeroPicccolo =array[i];
+            int posizioneValoreIndice=i;
+            
+            
+            
+                                                   //0  1  2  3   4 5 6 7 8   9
+           for (int j = 0; j < array.length; j++) {//25 14 45 32 12 3 5 6 89 23
+                      
+                if(numeroPicccolo>array[j]){
+                    
+                  numeroPicccolo=array[j];//3
+                  valoreAttuale=j;//5
+                }
+               
+                
+            }
+            numeroProvvisorio=array[i];
+            array[i]=numeroPicccolo;
+            array[valoreAttuale]=numeroProvvisorio;
+              }
+              for (int numeri:array) {
+            System.out.println(numeri);
+        }
+        
+        
+    }
+        
+    
+    
+}
